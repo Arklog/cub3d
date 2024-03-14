2c68a732dfbac655ff777d4e4df5a087bcaa7e57
Laurent LIAO <laliao@bess-f4r3s8.clusters.42paris.fr>
2024-03-14 17:56:23 +0100

    norme

--- src/raycast/draw_wall.c
+++ src/raycast/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: laliao <laliao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 11:04:41 by laliao            #+#    #+#             */
-/*   Updated: 2024/03/11 17:33:24 by laliao           ###   ########.fr       */
+/*   Updated: 2024/03/14 17:39:00 by laliao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /* if endian == 1, the Most Significant Byte(MSB) is leftmost */
 
-void	ft_put_pixel(t_img img, int x, int y, u_int32_t color, t_cub3d *cub3d)
+void	ft_put_pixel(t_img img, int x, int y, u_int32_t color)
 {
 	char	*pixel;
 	int		i;
@@ -29,15 +29,14 @@ void	ft_put_pixel(t_img img, int x, int y, u_int32_t color, t_cub3d *cub3d)
 			*pixel++ = (char)((color >> (img.bits_per_pixel - 8 - i)) & 0xFF);
 		i = i - 8;
 	}
-//	mlx_put_image_to_window(cub3d->mlx.mlx, cub3d->mlx.window, img.mlx_img, 0, 0);
 }
 
 /*x : column, y : top_wall(where we start to draw the top of the wall)*/
 
 void	ft_draw_wall(t_img img, int column, int top_wall, int pro_height, t_cub3d *cub, t_point wall)
 {
-	int	i;
-	int	tmp;
+	int		i;
+	int		tmp;
 	t_color	color;
 
 	i = 0;
@@ -45,7 +44,7 @@ void	ft_draw_wall(t_img img, int column, int top_wall, int pro_height, t_cub3d *
 	if (top_wall < 0)
 		top_wall = 0;
 	while (i < top_wall)
-		ft_put_pixel(img, column, i++, cub->textures[TEXTURE_CEILING].u_data.color.value, cub);
+		ft_put_pixel(img, column, i++, cub->textures[TEXTURE_CEILING].u_data.color.value);
 	i = 0;
 	while (i < pro_height && (top_wall + i) <= (WIN_HEIGHT - 1))
 	{
@@ -53,11 +52,11 @@ void	ft_draw_wall(t_img img, int column, int top_wall, int pro_height, t_cub3d *
 			color = get_pixel_colorf(cub, wall, fmin((double)(i + abs(tmp)) / (double)pro_height, 1));
 		else
 			color = get_pixel_colorf(cub, wall, fmin((double)(i) / (double)pro_height, 1));
-		ft_put_pixel(img, column, top_wall + i, color.value, cub);
+		ft_put_pixel(img, column, top_wall + i, color.value);
 		i++;
 	}
 	while (top_wall + i <= WIN_HEIGHT - 1)
-		ft_put_pixel(img, column, top_wall + i++, cub->textures[TEXTURE_FLOOR].u_data.color.value, cub);
+		ft_put_pixel(img, column, top_wall + i++, cub->textures[TEXTURE_FLOOR].u_data.color.value);
 }
 
 // function that calculate all the dimensions of the wall and the distance
@@ -72,8 +71,9 @@ void	ft_display(t_game *game, t_point wall, t_ray ray)
 	dist = distance(game->p1, wall);
 	if (ray.angle != game->p1.pov)
 		dist = dist * cos(ray.beta * (double)(M_PI / 180));
-	pro_height = floor(((WIN_LENGHT / 2) / tan(30 * (double)(M_PI / 180))) * 
-		(TILE / dist));
+	pro_height = floor(((WIN_LENGHT / 2) / tan(30 * (double)(M_PI / 180)))
+			* (TILE / dist));
 	top_wall = (WIN_HEIGHT / 2) - (pro_height / 2);
-	ft_draw_wall(game->img, ray.column, top_wall, pro_height, game->cub3d, wall);
+	ft_draw_wall(game->img, ray.column, top_wall, pro_height,
+		game->cub3d, wall);
 }
